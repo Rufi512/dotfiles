d0078c665725352fe597052984571c59fa838a10
Rufi <Rafajos9@gmail.com>
2023-01-15 19:10:46 -0400

    Changes playerctl.py

--- .config/qtile/settings/scripts/playerctl.py
+++ .config/qtile/settings/scripts/playerctl.py
@@ -1,26 +1,20 @@
 import subprocess
 
-def metadata(player='tauon'):
+def metadata(player=''):
 	try:
 		music_player = subprocess.run('playerctl --player=%s status --format "{{ uc(status) }}"' % (player),shell=True,stdout=subprocess.PIPE, text=True).stdout
-
 		if(music_player == ''):
 			return 'No player start'
 
-		status = subprocess.run('playerctl --player=%s status --format "{{ uc(status) }}"' % (player),shell=True,stdout=subprocess.PIPE, text=True).stdout.strip().upper()
-
-		track = subprocess.run('playerctl --player=tauon metadata --format "{{ duration(position) }} ^ {{ title }} ^ {{ artist }}"',shell=True,stdout=subprocess.PIPE, text=True).stdout.split('^')
-
-		if status != "PLAYING":
-			if status == "STOPPED":
-				metadata = f'Track Stopped - 🎵 {track[1].strip()+" -" if track[1].strip() else "Track unknow -"} {track[2].strip() if track[2].strip() else "Artist unknow"}'
+		track = subprocess.run('playerctl --player=%s metadata --format "{{ duration(position) }} ^ {{ title }} ^ {{ artist }} ^ {{status}}"' % (player),shell=True,stdout=subprocess.PIPE, text=True).stdout.split('^')
+		
+		if track[3].strip() != "Playing":
+			if track[3].strip() == "Stopped":
+				metadata = f'Track Stopped - 📻 {track[1].strip()+" -" if track[1].strip() else "Track unknow -"} {track[2].strip() if track[2].strip() else "Artist unknow"}'
 			else:
-				metadata = f'Track Paused - 🎵 {track[1].strip()+" -" if track[1].strip() else "Track unknow -"} {track[2].strip() if track[2].strip() else "Artist unknow"}'
-			
-
+				metadata = f'Track Paused - 📻 {track[1].strip()+" -" if track[1].strip() else "Track unknow -"} {track[2].strip() if track[2].strip() else "Artist unknow"}'
 		else:
-			metadata = f'🎵 {track[0].strip()} - {track[1].strip()+" -" if track[1].strip() else "Track unknow -"} {track[2].strip() if track[2].strip() else "Artist unknow"}'
-
+			metadata = f'📻 {track[1].strip()+" -" if track[1].strip() else "Track unknow -"} {track[2].strip() if track[2].strip() else "Artist unknow"}'
 		return metadata
 
 	except:
